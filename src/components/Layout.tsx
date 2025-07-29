8252c117e1c6ab05cd8aa0c84d95dc4a684eee96
ashaykubal <ashay.kubal@gmail.com>
2025-07-29 18:43:52 -0400

    Fix OverlayScrollbars Implementation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,29 +5,29 @@ import Navigation from './Navigation';
 
 const Layout: React.FC = () => {
   return (
-    <div className="min-h-screen bg-dark-bg text-gray-200">
-      <Navigation />
-      <OverlayScrollbarsComponent
-        options={{
-          scrollbars: {
-            autoHide: 'leave',
-            autoHideDelay: 800,
-            theme: 'os-theme-dark'
-          },
-          overflow: {
-            x: 'hidden',
-            y: 'scroll'
-          }
-        }}
-        className="min-h-screen"
-      >
-        <main className="min-h-screen pb-32 lg:pb-8 lg:pl-32 relative z-10">
+    <OverlayScrollbarsComponent
+      options={{
+        scrollbars: {
+          autoHide: 'leave',
+          autoHideDelay: 800,
+          theme: 'os-theme-dark'
+        },
+        overflow: {
+          x: 'hidden',
+          y: 'scroll'
+        }
+      }}
+      className="h-screen"
+    >
+      <div className="min-h-screen bg-dark-bg text-gray-200">
+        <Navigation />
+        <main className="pb-32 lg:pb-8 lg:pl-32 relative z-10">
           <div className="max-w-7xl mx-auto">
             <Outlet />
           </div>
         </main>
-      </OverlayScrollbarsComponent>
-    </div>
+      </div>
+    </OverlayScrollbarsComponent>
   );
 };
 
